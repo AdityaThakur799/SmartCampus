2e35a78e45b2a7dffa44dd3c5fa25d6dd08504b1
Aditya Thakur <adityathakur9348@gmail.com>
2026-06-21 01:37:08 +0530

    Enable WhiteNoise middleware for Heroku deployment

--- student_management_system_part_11-master/student_management_system/settings.py
+++ student_management_system_part_11-master/student_management_system/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     #===Enable Only Making Project Live on Heroku==
-     #'whitenoise.middleware.WhiteNoiseMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -151,7 +151,7 @@ EMAIL_FILE_PATH=os.path.join(BASE_DIR,"sent_mails")
 #
 
 #Enable Only Making Project Live on Heroku
-# STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
+ STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 # import dj_database_url
 # prod_db=dj_database_url.config(conn_max_age=500)
 # DATABASES['default'].update(prod_db)
